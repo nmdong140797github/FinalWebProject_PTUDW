8bc9d429fc0b07f0bc317ec986f453d2a96ffec8
LAI NGOC BAO <1512025@student.hcmus.edu.vn>
2018-05-21 16:57:15 +0700

    bổ sung cho chức năng cập nhật thông tin cá nhân

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -3,8 +3,17 @@ var customerRepo = require('../repos/customerRepo');
 
 var router = express.Router();
 
+function getString(date)
+{
+    var currentDate = (date.getDate() < 10 ? '0' : '') + date.getDate();
+    var currentMonth = (date.getMonth()+1 < 10 ? '0' : '') + (parseInt(date.getMonth())+1).toString();
+    var chuoi=date.getFullYear()+'-'+currentMonth+'-'+currentDate;
+    return chuoi;
+}
+
 router.get('/update', (req, res) => {
     customerRepo.single(req.query.id).then((result) => {
+        result.ngay_sinh=getString(result.ngay_sinh);
         var vm={
             person: result
         }
