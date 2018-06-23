f8527ae83eaa04bc283269ff462114b47d0d10ff
Chris Nguyen <nmdong140797github>
2018-06-23 12:14:14 +0700

    update giỏ hàng về handlebar giỏ hàng

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -4,8 +4,6 @@ var cartRepo = require('../repos/cartRepo'),
 
 var router = express.Router();
 router.get('/', (req, res) => {
-    console.log("helo cart");
-    console.log(req.session.cart);
     var arr_p = [];
     for (var i = 0; i < req.session.cart.length; i++) {
         var cartItem = req.session.cart[i];
@@ -16,8 +14,6 @@ router.get('/', (req, res) => {
     var items = [];
     var total=0;
     Promise.all(arr_p).then(result => {
-        console.log("ket qua items");
-        console.log(result);
         for (var i = result.length - 1; i >= 0; i--) {
             var pro = result[i][0];
             var item = {
--- middle-wares/handleLayout.js
+++ middle-wares/handleLayout.js
@@ -1,5 +1,6 @@
 var categoryRepo = require('../repos/categoryRepo');
 var producerRepo=require('../repos/producerRepo');
+var  productRepo = require('../repos/productRepo');
 
 module.exports = (req, res, next) => {
 
@@ -9,16 +10,42 @@ module.exports = (req, res, next) => {
 
 	var p1=categoryRepo.loadAll();
 	var p2=producerRepo.loadAll();
-	
+	if(req.session.isLogged==true)
+	{var arr_p = [];
+    for (var i = 0; i < req.session.cart.length; i++) {
+        var cartItem = req.session.cart[i];
+        var p = productRepo.single(cartItem.ProId);
+        arr_p.push(p);
+	}
+	var items = [];
+	var total=0;
+	var CartInfo;
+    Promise.all(arr_p).then(result => {
+        for (var i = result.length - 1; i >= 0; i--) {
+            var pro = result[i][0];
+            var item = {
+                Product: pro,
+                Quantity: req.session.cart[i].Quantity,
+                Amount: pro.gia * req.session.cart[i].Quantity
+            };
+            total=total+item.Amount;
+            items.push(item);
+        }
+
+        res.locals.CartInfo = {
+            items: items,
+            total: total
+        };
+	});
+}
 	Promise.all([p1, p2]).then(([rows1, rows2])=>{
 		res.locals.layoutVM={
 			categories: rows1,
 			producers: rows2,
 			isLogged: req.session.isLogged,
 			curUser: req.session.user,
-			isSearch: false
+			isSearch: false,
 		};
-
 		next();
 	});
 };
