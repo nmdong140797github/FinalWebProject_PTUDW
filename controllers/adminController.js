3631e2488f4ebac1dfb3802470dab9cb9b4d49e6
ginrex <dattgk97gmail.com>
2018-06-29 14:43:02 +0700

    final update

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -41,49 +41,6 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/receipt', (req, res) => {
-    receiptRepo.getAllReceipt().then(result => {
-        var receipts = [];
-        // console.log('results', result);
-        for (var i = result.length - 1; i >= 0; i--) {
-            var receipt = {
-                ma_ddh: result[i].ma_ddh,
-                ngay_lap: result[i].ngay_lap.toISOString().substring(0, 10),
-                sample: result[i].sample,
-                tong_tien: result[i].tong_tien,
-                so_luong_hang: result[i].so_luong_hang - 1,
-                showMore: result[i].so_luong_hang > 1 ? true : false,
-                trang_thai_don_hang: result[i].trang_thai_don_hang == 0 ? 'Đang giao hàng' : (result[i].trang_thai_don_hang == 2 ? 'Đã huỷ' : 'Giao hàng thành công'),
-                statusRow: result[i].trang_thai_don_hang == 0 ? 'warning' : (result[i].trang_thai_don_hang == 2 ? 'error' : 'success'),
-            }
-            receipts.push(receipt);
-        }
-
-        console.log('receipts', receipts);
-        var vm = {
-            receipts: receipts,
-            isLogged: req.session.isLogged,
-            isAdmin: req.session.isAdmin,
-            length: receipts && receipts.length > 0 ? true : false,
-            isAdminDashboard: true
-        };
-        res.locals.layoutVM = {
-            ...res.locals.layoutVM,
-            isLogged: true,
-            isSearch: false,
-            showNavBar: false,
-            showSideBar: false,
-            isAdmin: req.session.isAdmin,
-            numberCat: state.numberCat,
-            numberProduct: state.numberProduct,
-            numberProducer: state.numberProducer,
-            numberReceipt: state.numberReceipt
-        };
-        console.log(res.locals.layoutVM.countProducer);
-        res.render('receipt/personal', vm);
-    });
-});
-
 
 
 module.exports = router;
